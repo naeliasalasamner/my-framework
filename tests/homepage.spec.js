2e695c26bb49a02cba483e8120a1570180fcca04
Naelia Salas Amner <naeliasalas@gmail.com>
2026-06-05 16:36:13 +0200

    adding more test cases

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -1,36 +1,34 @@
 const { test, expect } = require('@playwright/test');
 const HomePage = require('../pages/HomePage');
 
-
-
-test('homapage loads and has correct title', async ({ page }) => {
+test.describe('homapage loads and has correct title', async ({ page }) => {
     const homePage = new HomePage(page)
     await homePage.goto('/');
     await expect(page).toHaveTitle(/naelia salas/);
 });
 
-test('clicking About nav link scrolls to about section', async ({ page}) => {
+test.describe('clicking About nav link scrolls to about section', async ({ page}) => {
     const homePage = new HomePage(page)
     await homePage.goto('/');
     await page.click('a[href="#about"]');
     await expect(page.locator('#about')).toBeVisible();
 });
 
-test('clicking Projects nav link scrolls to projects section', async ({ page }) => {
+test.describe('clicking Projects nav link scrolls to projects section', async ({ page }) => {
     const homePage = new HomePage(page)
     await homePage.goto('/');
     await page.click('a[href="#projects"]');
     await expect(page.locator('#projects')).toBeVisible();
 });
 
-test('clicking Contact nav link scrolls to projects section', async ({ page }) => {
+test.describe('clicking Contact nav link scrolls to projects section', async ({ page }) => {
     const homePage = new HomePage(page)
     await homePage.goto('/');
     await page.click('a[href="#contact"]');
     await expect(page.locator('#contact')).toBeVisible();
 });
 
-test('CV download button is visible and correct download attribute', async ({ page }) => {
+test.describe('CV download button is visible and correct download attribute', async ({ page }) => {
     const homePage = new HomePage(page)
     await page.goto('/');
     const downloadButton = page.locator('a[download="NaeliaSalasCv.pdf"]');
@@ -38,7 +36,7 @@ test('CV download button is visible and correct download attribute', async ({ pa
     await expect(downloadButton).toHaveAttribute('href', '/NaeliaSalasCv.pdf');
 });
 
-test('form shows error for invalid email', async ({ page }) => {
+test.describe('form shows error for invalid email', async ({ page }) => {
     const homePage = new HomePage(page)
     await homePage.goto('/');
     await page.fill('input[placeholder="Your name"]', 'Tester');
@@ -46,7 +44,7 @@ test('form shows error for invalid email', async ({ page }) => {
     await page.fill('textarea[placeholder="Your message"]', 'Hello');
     await page.click('button[type="submit"]');
     await expect(page.locator('text=Please enter a valid email address')).toBeVisible();
-});
+
 
 test('form shows error when GDPR is not checked', async ({ page }) => {
     const homePage = new HomePage(page);
@@ -54,5 +52,5 @@ test('form shows error when GDPR is not checked', async ({ page }) => {
     await homePage.fillContactForm('Tester', 'testtest.com', 'Hello');
     await homePage.submitForm();
     await expect(page.locator('text=You must agree to the GDPR terms')).toBeVisible();
+    });
 });
-
